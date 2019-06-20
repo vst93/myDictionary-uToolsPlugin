b99cc912861412463ac07495fb3245c713654ec1
vst <vst93g@gmail.com>
2019-06-21 01:49:20 +0800

    截取页面

--- assets/index.js
+++ assets/index.js
@@ -5,7 +5,7 @@ utools.onPluginEnter(({ code, type, payload }) => {
         utools.setExpendHeight(0);
         utools.setSubInput(({text}) => {
             this.text = text
-        }, "请输入需要查询的中英文词汇");
+        }, "请输入需要查询的中英文词汇");
     }
 });
 
@@ -24,18 +24,32 @@ function search_word(word){
     var append_html = ""
     var url = "https://m.youdao.com/dict?le=eng&q="+word;
     $.get(url, function(data){
-        var reg = /ec_contentWrp".*ul>(.*)<\/ul/gim;
-        var res = []
-        while (re = reg.exec(data)) {
-            res.push(re[1])
-        }
         t=data
-        console.log(res)
-        append_html = "";
-        res.forEach(function(u){ 
-            console.log(u) 
-            append_html  += u;            
-        })
+        append_html = '<h1>` '+word+' `</h1>';
+
+        //读音
+        var reg_audio = /英[\W\w]*?phonetic">([\W\w]*?)<\/span[\W\w]*?data-rel="([\W\w]*?)"[\W\w]*?美[\W\w]*?phonetic">([\W\w]*?)<\/span[\W\w]*?data-rel="([\W\w]*?)"/im;
+        var str_audio = reg_audio.exec(data)
+       
+        if(str_audio != null){
+            console.log(str_audio)
+            append_html += '<h3>英'+str_audio[1]+'&nbsp;&nbsp;&nbsp;美'+str_audio[3]+'</h3>';
+        }
+
+        //基础解释
+        var reg_ecContentWrp = /ec_contentWrp"[\W\w]*<ul>([\W\w]*?)<\/ul/im;
+        var str_ecCcontentWrp = reg_ecContentWrp.exec(data)
+        if(str_ecCcontentWrp != null){
+            append_html  += '<h2>释义</h2><ul>'+str_ecCcontentWrp[1]+'</ul>';       
+        }
+
+        //翻译
+        var reg_fanyiContentWrp = /fanyi_contentWrp"[\W\w]*?翻译结果[\W\w]*?trans-container[\W\w]*?<p>[\W\w]*?<\/p>([\W\w]*?)<\/p>/im;
+        var str_fanyiContentWrp = reg_fanyiContentWrp.exec(data)
+        if(str_fanyiContentWrp != null){
+            append_html  += '<h2>翻译</h2><p>'+str_fanyiContentWrp[1]+'</p>';       
+        }
+
         // $.each(data.items, function(i,item){
         //      append_html  += "<li><img onmouseenter=\"bigImg(this)\" src='"+item.picUrl+"' onerror=\"this.onerror='';src='assets/loading.gif'\" /></li>";
         //   });
