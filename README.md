439718c4044d05796e431323818e60e4247cd824
vst <vst93g@gmail.com>
2019-06-22 04:38:54 +0800

    界面与功能优化

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # myDictionary-uToolsPlugin
+中英文词语互译与查询，主要是utools现在的翻译功能没有具体单词的查询（比如音标）
+
+![](https://s2.ax1x.com/2019/06/22/Zpntu4.png)
+
+![](https://s2.ax1x.com/2019/06/22/ZpnNDJ.png)
+
+项目地址：https://github.com/vst93/myDictionary-uToolsPlugin
+
+##### 数据来源：有道词典 http://www.youdao.com
+
+#### 下载地址：
+链接: https://pan.baidu.com/s/1ei0xyPb9GNaJ0RdPHcng0w 提取码: nm3b
+
+
--- assets/index.js
+++ assets/index.js
@@ -1,19 +1,22 @@
 var text = ''
 var t
 utools.onPluginEnter(({ code, type, payload }) => {
+    utools.setExpendHeight(0);
     if(code == 'search_word'){
-        utools.setExpendHeight(0);
         utools.setSubInput(({text}) => {
             this.text = text
         }, "请输入需要查询的中英文词汇");
+        if(type=='over'){
+            utools.setSubInputValue(payload);
+            search_word(payload);
+        }
+       
     }
 });
 
 $(document).keydown(e => {
     switch (e.keyCode) {
         case 13:
-            utools.setExpendHeight(500);
-            $(".content").html('');
             search_word(text)
             break;
     }
@@ -21,19 +24,37 @@ $(document).keydown(e => {
 
 
 function search_word(word){
+    if(word==''){
+        return;
+    }
+    utools.setExpendHeight(544);
+    $(".content").html('');
     var append_html = ""
     var url = "https://m.youdao.com/dict?le=eng&q="+word;
     $.get(url, function(data){
         t=data
-        append_html = '<h1>` '+word+' `</h1>';
+        append_html = '<h1>'+word+'</h1>';
 
-        //读音
+        //读音 英/美
         var reg_audio = /英[\W\w]*?phonetic">([\W\w]*?)<\/span[\W\w]*?data-rel="([\W\w]*?)"[\W\w]*?美[\W\w]*?phonetic">([\W\w]*?)<\/span[\W\w]*?data-rel="([\W\w]*?)"/im;
         var str_audio = reg_audio.exec(data)
        
         if(str_audio != null){
-            console.log(str_audio)
-            append_html += '<h3>英'+str_audio[1]+'&nbsp;&nbsp;&nbsp;美'+str_audio[3]+'</h3>';
+            //append_html += '<h3 class="phonetic">英'+str_audio[1]+'<button class="play-phonetic-btn" type="button" onclick="playVoice(\''+str_audio[2]+'\');"></button>&nbsp;&nbsp;&nbsp;美'+str_audio[3]+'<button class="play-phonetic-btn" type="button" onclick="playVoice(\''+str_audio[4]+'\');"></button></h3>';
+            append_html += '<h3 class="phonetic">英'+str_audio[1]
+            +'<audio controls="controls" id="audio_player_1" style="display:none;" src="'+str_audio[2]+'"></audio>'
+            +'<button class="play-phonetic-btn" type="button" onclick="playVoice(\'audio_player_1\')"></button>&nbsp;&nbsp;&nbsp;美'+str_audio[3]
+            +'<audio controls="controls" id="audio_player_2" style="display:none;" src="'+str_audio[4]+'"></audio>'
+            +'<button class="play-phonetic-btn" type="button" onclick="playVoice(\'audio_player_2\')"></button></h3>';
+        }else{
+            //读音 单个读音
+            var reg_audio = /phonetic">([\W\w]*?)<\/span[\W\w]*?data-rel="([\W\w]*?)"/im;
+            var str_audio = reg_audio.exec(data)
+            if(str_audio != null){
+                console.log(str_audio)
+                append_html += '<h3 class="phonetic">'+str_audio[1]+'<button class="play-phonetic-btn" type="button" onclick="playVoice(\''+str_audio[2]+'\');"></button></h3>';
+            }
+
         }
 
         //基础解释
@@ -47,7 +68,7 @@ function search_word(word){
         var reg_fanyiContentWrp = /fanyi_contentWrp"[\W\w]*?翻译结果[\W\w]*?trans-container[\W\w]*?<p>[\W\w]*?<\/p>([\W\w]*?)<\/p>/im;
         var str_fanyiContentWrp = reg_fanyiContentWrp.exec(data)
         if(str_fanyiContentWrp != null){
-            append_html  += '<h2>翻译</h2><p>'+str_fanyiContentWrp[1]+'</p>';       
+            append_html  += '<h2>翻译</h2><ul><li>'+str_fanyiContentWrp[1]+'</li></ul>';       
         }
 
         // $.each(data.items, function(i,item){
@@ -62,4 +83,13 @@ function filterATag(msg) {
     var msg = msg.replace(/<a[\W\w]*?>/gim, '');
     msg = msg.replace('</a>', '')
     return msg;
+}
+
+
+function playVoice2(file) {
+    $('#voice').html('<audio controls="controls" id="audio_player" style="display:none;"> <source src="' + file + '" > </audio><embed id="MPlayer_Alert" src="' + file + '" loop="false" width="0px" height="0px" /></embed>');
+}
+
+function playVoice(id_name){
+    document.getElementById(id_name).play();
 }
